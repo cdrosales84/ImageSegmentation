5cebc0ad3fd27e356a1b4eec7b35dd3c2c6c9ab2
cdrosales84 <cdrosales84@gmail.com>
2024-03-20 21:26:27 -0300

    Varias Funciones

--- data.py
+++ data.py
@@ -10,7 +10,7 @@ from albumentations import HorizontalFlip, VerticalFlip, ElasticTransform, GridD
 def create_dir(path):
     if not os.path.exists(path):
         try:
-            os.mkdirs(path)
+            os.makedirs(path)
         except:
             print("Error creating directory")
         
@@ -30,9 +30,57 @@ def augment_data(images, masks, save_path, augment=True):
     
     for idx, (x, y) in tqdm(enumerate(zip(images, masks)), total = len(images), desc="Progresing..."):
         """ Extracting names """
-        #name = x.split()
-        print(x, y)
-        break
+        name = x.split("\\")[-1].split('.')[0]
+        """ Reading Images and Masks"""
+        x = cv2.imread(x, cv2.IMREAD_COLOR)
+        y = imageio.mimread(y)[0]
+        x = cv2.resize(x, (W, H))
+        y = cv2.resize(y, (W, H))
+        
+        if augment == True:
+            aug = HorizontalFlip(p=1.0)
+            augmented = aug(image=x, mask=y)
+            x1 = augmented["image"]
+            y1 = augmented["mask"]
+
+            aug = VerticalFlip(p=1.0)
+            augmented = aug(image=x, mask=y)
+            x2 = augmented["image"]
+            y2 = augmented["mask"]
+
+            aug = ElasticTransform(p=1, alpha=120, sigma=120 * 0.05, alpha_affine=120 * 0.03)
+            augmented = aug(image=x, mask=y)
+            x3 = augmented['image']
+            y3 = augmented['mask']
+
+            aug = GridDistortion(p=1)
+            augmented = aug(image=x, mask=y)
+            x4 = augmented['image']
+            y4 = augmented['mask']
+
+            aug = OpticalDistortion(p=1, distort_limit=2, shift_limit=0.5)
+            augmented = aug(image=x, mask=y)
+            x5 = augmented['image']
+            y5 = augmented['mask']
+
+            X = [x, x1, x2, x3, x4, x5]
+            Y = [y, y1, y2, y3, y4, y5]
+        else:
+            X = [x]
+            Y = [y]
+        index = 0
+        for i, m in zip(X, Y):
+            temp_image_name = f"{name}_{index}.jpg"
+            temp_mask_name = f"{name}_{index}.jpg"
+            image_path = os.path.join(save_path, "image", temp_image_name)
+            mask_path = os.path.join(save_path, "mask", temp_mask_name)
+                        
+            cv2.imwrite(image_path, i)                
+            cv2.imwrite(mask_path, m)
+            index += 1
+        
+                                    
+
 
 if __name__ == "__main__":
     """ Seeding"""
@@ -45,13 +93,10 @@ if __name__ == "__main__":
     print(f"Train: {len(train_x)} - {len(train_y)}")
     print(f"Train: {len(test_x)} - {len(test_y)}")
   
-    """ Create Directories """
-    
-    create_dir(os.path.join(os.getcwd(), "/new_data"))
-
-    """
-    create_dir(os.getcwd()+"new_data\train\mask")
-    create_dir(os.getcwd()+"new_data\test\image")
-    create_dir(os.getcwd()+"new_data\test\mask")
-    """
-   # augment_data(train_x, train_y, "new_data/train/", augment=True)
+    create_dir("new_data/train/image")
+    create_dir("new_data/train/mask")
+    create_dir("new_data/test/image")
+    create_dir("new_data/test/mask")
+  
+    augment_data(train_x, train_y, "new_data/train/", augment=True)
+    augment_data(test_x, test_y, "new_data/test/", augment=False)
